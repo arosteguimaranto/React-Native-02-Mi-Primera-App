029b4eb6fb0ec8b3f897535a2ecbe5696648a85d
arosteguimaranto <arosteguimaranto@outlook.com>
2023-12-04 17:20:33 -0600

    Hola Mundo en ReactNative

---
+++ 02-MiPrimeraApp/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { View, Text  } from 'react-native'
+
+ const App = () => {
+  return (
+    <View style={{
+      flex: 1,
+      backgroundColor: 'pink',
+      justifyContent: 'center'
+
+    }}>
+      <Text
+      style={{
+        fontSize: 45,
+        textAlign:'center'
+      }}>
+
+        Hola Mundo
+      </Text>
+    </View>
+  )
+}
+
+
+export default App;
