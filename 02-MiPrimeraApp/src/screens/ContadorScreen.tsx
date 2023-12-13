319ae2ea96566361c495ee06f33b3240c0c12bf6
arosteguimaranto <arosteguimaranto@outlook.com>
2023-12-13 14:22:05 -0600

    Creando un contador

--- 02-MiPrimeraApp/src/screens/ContadorScreen.tsx
+++ 02-MiPrimeraApp/src/screens/ContadorScreen.tsx
@@ -1,9 +1,27 @@
-import { Text, View } from "react-native"
+import { useState } from "react"
+import { Button, Text, View } from "react-native"
 
 export const ContadorScreen = () => {
-  return (
-    <View>
-        <Text> Contador: xxxx</Text>
-    </View>
-  )
+    const [contador, setContador] = useState(10)
+    return (
+        <View style={{
+            flex: 1,
+            justifyContent: 'center'
+        }}>
+            <Text
+                style={{
+                    textAlign: 'center',
+                    fontSize: 40,
+                    top: -15
+                }}
+            > Contador: {contador}
+            </Text>
+            <Button
+            title="Click"
+            onPress={() =>setContador(contador + 1)}
+            
+            />
+
+        </View>
+    )
 }
