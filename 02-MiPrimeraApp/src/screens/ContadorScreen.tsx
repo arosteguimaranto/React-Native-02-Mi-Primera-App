184bd189470753bd1d2d7ec5bf22c259d2e8c0ab
arosteguimaranto <arosteguimaranto@outlook.com>
2023-12-18 17:57:13 -0600

    Componente personalizado: FAB - Floating Action Button

--- 02-MiPrimeraApp/src/screens/ContadorScreen.tsx
+++ 02-MiPrimeraApp/src/screens/ContadorScreen.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import {  TouchableOpacity, Text, View, StyleSheet } from "react-native"
+import { Fab } from "../components/Fab"
 
 export const ContadorScreen = () => {
     const [contador, setContador] = useState(10)
@@ -9,13 +10,30 @@ export const ContadorScreen = () => {
                  Contador:{contador}
             </Text>
 
-           <TouchableOpacity
+            <Fab
+            title='+1'
+            />
+
+          {/*
+            <TouchableOpacity
+           style={styles.fabLocationBR}
            onPress={()=>setContador(contador +1)}
            >
-            <View style={styles.bottonIncrementar}>
-                <Text> +1 </Text>
+            <View style={styles.fab}>
+                <Text style={styles.fabText}> +1 </Text>
             </View>
            </TouchableOpacity>
+           <TouchableOpacity
+           style={styles.fabLocationBL}
+           onPress={()=>setContador(contador -1)}
+           >
+            
+            <View style={styles.fab}>
+                <Text style={styles.fabText}> -1 </Text>
+            </View>
+           </TouchableOpacity>
+           */
+          } 
 
         </View>
     )
@@ -34,5 +52,29 @@ const styles = StyleSheet.create({
     bottonIncrementar:{
         backgroundColor:'red',
         borderRadius: 100
+    },
+    fabLocationBR:{
+        position:'absolute',
+        bottom: 25,
+        right: 25 
+    },
+    fabLocationBL:{
+        position: 'absolute',
+        bottom: 25,
+        left: 25
+    },
+    fab:{
+        backgroundColor:'#5856D6',
+        width: 60,
+        height: 60,
+        borderRadius: 100,
+        justifyContent: 'center'
+    },
+    fabText: {
+        color: 'white',
+        fontSize: 25,
+        fontWeight: 'bold',
+        alignSelf:'center'
     }
+
 })
